479c9e39a15850d8afb76cd2ac8d8e77abd13740
David Hughes <d.hughes33@live.com>
2019-10-27 02:11:51 -0700

    add getters/setters for unit testing

--- src/main/java/com/cs5800/calculator/Calculator.java
+++ src/main/java/com/cs5800/calculator/Calculator.java
@@ -5,25 +5,29 @@ import java.util.Scanner;
 /**
  * Calculator
  */
-public class Calculator
-{
+public class Calculator {
     private TokenStack operatorStack;
     private TokenStack operandStack;
     private static Scanner inputScanner = new Scanner(System.in);
 
     Calculator() {
-        operatorStack = new TokenStack();
-        operandStack = new TokenStack();
+        setOperatorStack(new TokenStack());
+        setOperandStack(new TokenStack());
     }
 
-    public double evaluate(String input){
+
+
+    public double evaluate(String input) {
         double result = 10.0;
         
+        Token[] inputTokens = new Tokenizer(input).getTokens();
+
+
         return result;
     }
 
 
-    
+
     public static void main(String[] args) {
         System.out.println("Enter expression with components seperated by spaces: ");
         String input = inputScanner.nextLine();
@@ -31,5 +35,21 @@ public class Calculator
         Calculator calculator = new Calculator();
         double result = calculator.evaluate(input);
         System.out.println(result);
+    }
+
+
+
+    //getters/setters for unit testing
+    public TokenStack getOperatorStack() {
+        return operatorStack;
+    }
+    public void setOperatorStack(TokenStack operatorStack) {
+        this.operatorStack = operatorStack;
+    }
+    public TokenStack getOperandStack() {
+        return operandStack;
+    }
+    public void setOperandStack(TokenStack operandStack) {
+        this.operandStack = operandStack;
     }
 }
