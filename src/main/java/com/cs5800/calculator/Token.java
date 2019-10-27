6ab32b36a83cc0533c2f611f81a9ded622e9ffcb
David Hughes <d.hughes33@live.com>
2019-10-26 21:44:55 -0700

    Intial commit of Token class and test class

---
+++ src/main/java/com/cs5800/calculator/Token.java
@@ -0,0 +1,9 @@
+package com.cs5800.calculator;
+
+/**
+ * Token
+ */
+public class Token
+{
+
+}
---
+++ src/test/java/com/cs5800/calculator/TokenTest.java
@@ -0,0 +1,12 @@
+package com.cs5800.calculator;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Unit test for tokenizer.
+ */
+public class TokenTest
+{
+
+}
