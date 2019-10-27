5faefd7ea864c36ae9f3b4d27a0f7799fed4d934
David Hughes <d.hughes33@live.com>
2019-10-27 00:02:48 -0700

    Add constructor and getter

--- src/main/java/com/cs5800/calculator/Tokenizer.java
+++ src/main/java/com/cs5800/calculator/Tokenizer.java
@@ -5,5 +5,18 @@ package com.cs5800.calculator;
  */
 public class Tokenizer
 {
+    Token[] tokens;
 
+    Tokenizer(String input){
+        String[] splitBySpaces = input.trim().split(" ");
+        tokens = new Token[splitBySpaces .length];
+
+        for(int i = 0; i < splitBySpaces.length; i++){
+            tokens[i] = new Token(splitBySpaces[i]);
+        }
+    }
+
+    public Token[] getTokens(){
+        return tokens;
+    }
 }
