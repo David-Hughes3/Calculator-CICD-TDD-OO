bb265dd5623b664a1e7e9e238910693208d3d0d9
David Hughes <d.hughes33@live.com>
2019-10-26 22:15:17 -0700

    Handle both OPERAND and OPERATOR token types

--- src/main/java/com/cs5800/calculator/Token.java
+++ src/main/java/com/cs5800/calculator/Token.java
@@ -1,17 +1,43 @@
 package com.cs5800.calculator;
 
+import java.util.regex.Pattern;
+
 /**
  * Token
  */
 public class Token
 {
     public enum TYPE{
-        OPERAND;
+        OPERAND, OPERATOR;
     }
 
     TYPE type;
+    private double operand;
+    private char operator;
 
-    Token(String input){
-        type = TYPE.OPERAND;
+    Token(String input) throws ExceptionInInitializerError {
+
+        if(isDouble(input)){
+            type = TYPE.OPERAND;
+        }
+        else if(input=="+" || input=="-" || input=="/" || input=="*" || input=="%"){
+            type = TYPE.OPERATOR;
+        }
+        else{
+            throw new ExceptionInInitializerError("Unknown String Passed to Be Token");
+        }
+        
+    }
+
+    private boolean isDouble(String str){
+        try{
+            double x = Double.parseDouble(str);
+            return true;
+        }
+        catch(NumberFormatException e){
+            return false;
+        }
+        
+        
     }
 }
