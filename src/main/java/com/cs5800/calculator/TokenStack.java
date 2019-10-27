3d4efcc4ea7c6b8c577b71d17ca0e2efeb0a367c
David Hughes <d.hughes33@live.com>
2019-10-27 00:28:17 -0700

    Add code to pass TokenStackTest tests; simple stack

--- src/main/java/com/cs5800/calculator/TokenStack.java
+++ src/main/java/com/cs5800/calculator/TokenStack.java
@@ -1,9 +1,31 @@
 package com.cs5800.calculator;
 
+import java.util.ArrayDeque;
+
 /**
  * TokenStack
  */
 public class TokenStack
 {
+    private ArrayDeque<Token> tokens;
 
+    public TokenStack(){
+        tokens = new ArrayDeque<Token>();
+    }
+
+    public boolean isEmpty(){
+        return tokens.isEmpty();
+    }
+
+    public Token peek() {
+        return tokens.peek();
+    }
+
+    public void push(Token token){
+        tokens.push(token);
+    }
+
+    public Token pop(){
+        return tokens.pop();
+    }
 }
