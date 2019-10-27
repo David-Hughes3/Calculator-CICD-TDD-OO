1407a6846ce640799640e45e741a0d4d41d02557
David Hughes <d.hughes33@live.com>
2019-10-27 00:27:42 -0700

    Add tests for a TokenStack class

--- src/test/java/com/cs5800/calculator/TokenStackTest.java
+++ src/test/java/com/cs5800/calculator/TokenStackTest.java
@@ -1,8 +1,13 @@
 package com.cs5800.calculator;
 
-
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
 import static org.junit.Assert.*;
+
+import java.util.NoSuchElementException;
+
+import org.junit.Rule;
 
 /**
  * Unit test for TokenStack
@@ -10,5 +15,67 @@ import static org.junit.Assert.*;
 public class TokenStackTest
 {
 
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+
+
+    @Test 
+    public void checkPopAndPush(){
+        Token a = new Token("7");
+        Token b = new Token("+");
+        TokenStack tokenStack = new TokenStack();
+        tokenStack.push(b);
+        tokenStack.push(a);
+
+        Token result = tokenStack.pop();
+        assertEquals(a, result);
+
+        result = tokenStack.pop();
+        assertEquals(b, result);
+    }
+
+    @Test
+    public void checkPopForNoSuchElementException(){
+        Token a = new Token("7");
+        TokenStack tokenStack = new TokenStack();
+        tokenStack.push(a);
+
+        Token result = tokenStack.pop();
+        assertEquals(a, result);
+
+        thrown.expect(NoSuchElementException.class);
+        result = tokenStack.pop();
+    }
+
+    @Test
+    public void checkIsEmpty(){
+        Token a = new Token("7");
+        TokenStack tokenStack = new TokenStack();
+        tokenStack.push(a);
+
+        boolean result = tokenStack.isEmpty();
+        assertEquals(false, result);
+
+        tokenStack.pop();
+        result = tokenStack.isEmpty();
+        assertEquals(true, result);
+    }
+
+    @Test 
+    public void checkPeek(){
+        Token a = new Token("7");
+        Token b = new Token("+");
+        TokenStack tokenStack = new TokenStack();
+        tokenStack.push(b);
+        tokenStack.push(a);
+
+        Token result = tokenStack.peek();
+        assertEquals(a, result);
+
+        tokenStack.pop();
+        result = tokenStack.peek();
+        assertEquals(b, result);
+    }
 
 }
