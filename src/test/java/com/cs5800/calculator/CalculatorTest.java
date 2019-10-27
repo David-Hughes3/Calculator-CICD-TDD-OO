c92c4f5726b7aac3333615d7c2aee4050cac9bb7
David Hughes <d.hughes33@live.com>
2019-10-27 03:30:35 -0700

    Add tests for many of same op in a row and verifying precedence

--- src/test/java/com/cs5800/calculator/CalculatorTest.java
+++ src/test/java/com/cs5800/calculator/CalculatorTest.java
@@ -31,6 +31,76 @@ public class CalculatorTest
         assertEquals((double)(4), result, 0.01);
     }
 
+    @Test
+    public void checkEvaluationOfComplexExpressions(){
+        Calculator c = new Calculator();
+        double result = c.evaluate("7 + 3");
+        assertEquals((double)(10), result, 0.01);
+
+        result = c.evaluate("7 - 3");
+        assertEquals((double)(4), result, 0.01);
+    }
+
+    @Test
+    public void checkForCorrectPrecedence(){
+        Calculator c = new Calculator();
+        double result = c.evaluate("7 * 3 + 12 / 4");
+        assertEquals((double)(24), result, 0.01);
+
+        result = c.evaluate("7 + 3 / 3 - 3 * 3");
+        assertEquals((double)(-1), result, 0.01);
+    }
+
+    @Test
+    public void checkManyAdds(){
+        Calculator c = new Calculator();
+        double result = c.evaluate("7 + 3 + 4");
+        assertEquals((double)(14), result, 0.01);
+
+        result = c.evaluate("7 + 3 + 4 + 10 + 30 + 40000");
+        assertEquals((double)(40054), result, 0.01);
+    }
+
+    @Test
+    public void checkManySubs(){
+        Calculator c = new Calculator();
+        double result = c.evaluate("7 - 3 - 4");
+        assertEquals((double)(0), result, 0.01);
+
+        result = c.evaluate("0 - 7 - 3 - 12 - 15");
+        assertEquals((double)(-37), result, 0.01);
+    }
+
+    @Test
+    public void checkManyDivides(){
+        Calculator c = new Calculator();
+        double result = c.evaluate("20 / 5 / 2");
+        assertEquals((double)(2), result, 0.01);
+
+        result = c.evaluate("4 / 6 / 2 / 2");
+        assertEquals((double)(0.16), result, 0.01);
+    }
+
+    @Test
+    public void checkManyMultiplies(){
+        Calculator c = new Calculator();
+        double result = c.evaluate("5 * 4 * 2");
+        assertEquals((double)(40), result, 0.01);
+
+        result = c.evaluate("69 * 210 * 2 * 2 * 4");
+        assertEquals((double)(231840), result, 0.01);
+    }
+
+    @Test
+    public void checkManyModulus(){
+        Calculator c = new Calculator();
+        double result = c.evaluate("20 % 3 % 3");
+        assertEquals((double)(2), result, 0.01);
+
+        result = c.evaluate("100 % 9 % 1");
+        assertEquals((double)(0), result, 0.01);
+    }
+
     @After
     public void cleanUpStreams() {
         System.setOut(null);
