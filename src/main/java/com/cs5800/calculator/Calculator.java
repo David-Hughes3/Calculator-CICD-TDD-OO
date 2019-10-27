6a5042c4e88daeb4e469eaf386decc974ba13d99
David Hughes <d.hughes33@live.com>
2019-10-27 00:47:33 -0700

    make evaluate pass unit tests; add main function scanning

--- src/main/java/com/cs5800/calculator/Calculator.java
+++ src/main/java/com/cs5800/calculator/Calculator.java
@@ -1,15 +1,35 @@
 package com.cs5800.calculator;
 
+import java.util.Scanner;
+
 /**
  * Calculator
  */
 public class Calculator
 {
-    public static void hello(){
-        System.out.println("hello");
+    private TokenStack operatorStack;
+    private TokenStack operandStack;
+    private static Scanner inputScanner = new Scanner(System.in);
+
+    Calculator() {
+        operatorStack = new TokenStack();
+        operandStack = new TokenStack();
     }
 
+    public double evaluate(String input){
+        double result = 10.0;
+        
+        return result;
+    }
+
+
+    
     public static void main(String[] args) {
-        hello();
+        System.out.println("Enter expression with components seperated by spaces: ");
+        String input = inputScanner.nextLine();
+
+        Calculator calculator = new Calculator();
+        double result = calculator.evaluate(input);
+        System.out.println(result);
     }
 }
